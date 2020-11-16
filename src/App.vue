5201b853d70064b10d13c96d29eee88b0cb8c8bf
MRSlothAlan <tingkwokpoon@gmail.com>
2020-11-16 22:27:53 +0800

    created a template for the pop up for the promotion

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <b-modal ref="my-modal" hide-footer title="Using Component Methods">
+        <div class="d-block text-center">
+        <h3>This is the promotional material</h3>
+      </div>
+      <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
+    </b-modal>
     <NavBar />
     <router-view />
   </div>
@@ -7,9 +13,19 @@
 
 <script>
 import NavBar from './views/navBar.vue';
-
 export default {
   name: 'App',
+  methods: {
+      showModal() {
+        this.$refs['my-modal'].show()
+      },
+      hideModal() {
+        this.$refs['my-modal'].hide()
+      }
+    },
+    mounted() {
+      this.showModal();
+  },
   components: {
     NavBar,
   }
---
+++ src/components/promotion/promotion.vue
@@ -0,0 +1,31 @@
+<!-- This is the page for the promotion -->
+<template>
+    <div id="novin-alert-container">
+        <div v-for="message in messages" v-bind:key="message.id" class="alert novin-alert" :class="message.class">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            {{ message.body }}
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+                messages: [],
+            }
+        },
+        methods: {
+            SelfAlert: function (message) {
+                let app = this;
+                message.type = typeof message.type !== 'undefined' ? message.type : 'info';
+                message.class = 'alert' + message.type;
+                let index = app.messages.push(message) - 1;
+                if (typeof message.duration !== 'undefined') {
+                    window.setTimeout(function () {
+                        app.messages.splice(index, 1);
+                    }, message.duration * 1000);
+                }
+            }
+        }
+    }
+</script>
