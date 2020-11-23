8d729f217a1113404e05399ef53e4994db2e122b
MRSlothAlan <tingkwokpoon@gmail.com>
2020-11-24 01:02:47 +0800

    template for the promotion pop-up

---
+++ src/components/promotion/promotion.vue
@@ -0,0 +1,21 @@
+<!-- This is the page for the promotion -->
+<template>
+    <div id="promotion_template">
+        <h3>#CityHack21 is comming!</h3>
+        <!-- Place the image in here -->
+        <!-- Maybe add some links to subpages? -->
+        <a href="../landingPage/aboutUs.vue">
+            <img src="../../assets/image/promotion_poster/img1.jpg"/>
+        </a>
+    </div>
+</template>
+<script>
+    // import vuetify from '../../plugins/vuetify.js';
+    export default {
+        name: "PromotionMaterial"
+    }
+
+</script>
+<style>
+
+</style>
