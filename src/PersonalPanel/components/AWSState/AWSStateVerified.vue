ef49b3d6082286fafcad58507eb43e23660cee2a
MRSlothAlan <tingkwokpoon@gmail.com>
2020-12-19 21:35:25 +0800

    here are the messages. Just a template, I will modify the default icons.

---
+++ src/PersonalPanel/components/AWSState/AWSStateVerified.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-alert 
+        transition="scale-transition"
+        dense 
+        text 
+        type="success" 
+        id="alertAWSVerified">
+        AWS Educate Account has already been verified
+    </v-alert>
+</template>
+
+<script>
+    export default {
+        name: "AWSVerified"
+    }
+</script>
+<style lang="scss" scoped>
+#alertAWSVerified {
+    text-align: center;
+    position: relative;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 600px;
+}
+
+</style>
