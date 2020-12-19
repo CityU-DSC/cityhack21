91fa85e8577fa7b3323d49f962c59b57f30db171
Luke <lukehsu1999@gmail.com>
2020-12-19 13:10:43 +0800

    Auto stash before merge of "develop" and "origin/develop"

--- src/PersonalPanel/components/AWSVerification.vue
+++ src/PersonalPanel/components/AWSVerification.vue
@@ -2,6 +2,21 @@
   <div>
     <nav-drawer @direct="directTo"/>
   </div>
+<div>
+    <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <h2 class="mb-5">What is Hackathon</h2>
+              <p>
+                The word “hackathon” is a portmanteau of “hacking marathon”.
+              </p>
+              <p>
+                A hackathon is a design sprint-like event which involves parties with software development background such as computer programmers, graphic designers, interface designers, project managers, and others who collaborate intensively on software projects. The goal of the hackathon is to create functioning software or hardware before the end of the event.
+              </p>
+            </v-card-text>
+          </v-card>
+    </v-tab-item>
+</div>
 </template>
 
 <script>
