dacff7a52b1af72f055e4b5acc9774252ca4dde2
ryanyen2 <ryanyen2-c@my.cityu.edu.hk>
2020-12-21 19:16:09 +0800

    add aws info

--- src/PersonalPanel/components/AWSVerification.vue
+++ src/PersonalPanel/components/AWSVerification.vue
@@ -1,31 +1,41 @@
 <template>
 <div>
   <nav-drawer @direct="directTo" :current="pathName"/>
-
-  <br>
-
-  <div>
+  <v-col>
+    <v-carousel
+        cycle
+        height="400"
+        hide-delimiter-background
+        show-arrows-on-hover
+    >
+      <v-carousel-item
+          v-for="(slide, i) in slides"
+          :key="i"
+      >
+        <v-sheet
+            :color="colors[i]"
+            height="100%"
+        >
+          <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+          >
+            <div class="display-3">
+              {{ slide }}
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </v-col>
+  <div class="mt-5">
     <v-row justify="space-around">
       <v-card width="400">
         <v-img
           height="200px"
           :src="submissionDetail.imageUrl"
         >
-          <v-app-bar
-            flat
-            color="rgba(0, 0, 0, 0)"
-          >
-            <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
-
-            <v-spacer></v-spacer>
-
-            <v-btn
-              color="white"
-              icon
-            >
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </v-app-bar>
         </v-img>
 
         <v-card-text v-if="submissionDetail.imageUrl">
@@ -102,7 +112,20 @@ export default {
       imgUrls: [],
       submissionDetail: {
         imageUrl: null,
-      }
+      },
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [
+        'Multiple Gifts From AWS Educate',
+        'Unique AWS Souvenirs',
+        'Free 100USD Credits',
+        'Using SageMaker in CityHack',
+      ],
     };
   },
   created() {
@@ -193,7 +216,7 @@ export default {
             console.error(err);
             swal("Error", "Something Went Wrong", "error");
           }
-          
+
         });
       });
       //this.getImages();
@@ -201,14 +224,14 @@ export default {
     async awsVerifyUrl(){
       try{
         await this.awsVerify(
-          { imageUrl:this.submissionDetail.imageUrl }, 
+          { imageUrl:this.submissionDetail.imageUrl },
           localStorage.getItem("jwt")
           )
         .then(res =>{
           console.log("awsVerification: ");
           console.log(res);
         })
-      } 
+      }
       catch(err){
         swal("Error", "Something Went Wrong", "error");
         console.log(err.response);
