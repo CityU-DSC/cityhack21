a99863c234e9c00e946b2e50c4dfe88e296ce40e
Michelle <wuqianyi0118@gmail.com>
2021-01-07 22:06:02 +0800

    feat: theme description

--- src/components/landingPage/aboutUs.vue
+++ src/components/landingPage/aboutUs.vue
@@ -90,14 +90,52 @@
               <p>
                 This year we will have open-ended tasks. Participants can think of possible solutions or ideas to improve the situation caused by Covid19. Below are the four main aspects that they can consider in their solutions:
               </p>
-              <ol class="ml-3">
+              <!-- <ol class="ml-3">
                 <li>WFH, online meeting, online learning</li>
                 <li>Resource allocation</li>
                 <li>Reduce direct contact and promote social distancing </li>
                 <li>Safety precautions</li>
-              </ol>
+              </ol> -->
             </v-card-text>
           </v-card>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header><p class="ml-3">WFH, online meeting, online learning</p></v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <p class="ml-3 grey-color">COVID 19 has had a strong negative effect on economies around the world. However, it has also been a catalyst for great positive change. Many organizations have developed robust systems for keeping adequate levels of productivity outside of the office area. And that allowed professionals and students to stay productive at home. </p>
+                <p class="ml-3 grey-color">Work from home arrangement has its challenges, for instance, the difficulty of managing tasks and inefficient collaboration while being at a distance from colleagues. These challenges are partly being solved through intelligent use of technology such as online conferencing, task managing software, corporate messaging portals, but unfortunately, the aforementioned tools leave a lot to be desired. </p>
+                <p class="ml-3 grey-color">Innovative solutions, developed by CityHack contestants, could make a home office a viable substitute for an office or a university campus.</p>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header><p class="ml-3">Resource allocation</p></v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <p class="ml-3 grey-color">COVID 19 has slowed the traffic system and the whole world. In this critical period, any resources have to be appreciated and be well allocated to make use of the limited to the maximum. In your solution/project, get resource allocation into your mind. The resources we are talking about here can be life attached, such as food, toilet paper, masks, and other everyday necessities. It can also be medical-related, such as medicine, medical gloves, or other medical supplies. It can be human resources, such as task management with geological limits, as we cannot travel around at this moment. It can also be services such as delivery, quarantine hotel vacancies, or even the quotas of testing. </p>
+                <p class="ml-3 grey-color">Not only do you need to consider the availability of the resources, but also the priority and how it can be accessed. If there are two hospitals that both have a doctor in need but there is only one allocatable, where should we send the doctor to? The one with more cases, or the closest one? You also need to think about the access processes, that can be, how to notify the doctor and the hospital? How to deliver the doctor to the designated location? How to make sure the doctor can arrive on time? </p>
+                <p class="ml-3 grey-color">There is always more to dig into when it comes to resource allocation. For example, if you are trying to build up your solution with an independent online meeting service, you will also need to consider the allocation of data storage, network connection, and access to the service. Sometimes a trade-off policy is needed if you have already maximized the efficiency. Thinking about the subtleties makes the project more complete. </p>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header><p class="ml-3">Reduce direct contact and promote social distancing</p></v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <p class="ml-3 grey-color">The outbreak of Covid-19 has hugely damaged global business and the economy. Providing contactless services has been vitally important to getting the industry back on its feet and encouraging consumers to use/buy the services and products. This topic is for those who would like to come up with contactless technologies that can help various industries to overcome the challenges of Covid-19 and raise public awareness about social distancing. Please provide solutions to minimize the risk of exposure to the virus such as self-service systems, online payment, online taxi booking service, and real-time public transportation service. </p>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header><p class="ml-3">Safety precautions</p></v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <p class="ml-3 grey-color">Covid-19 has been an incredible demonstrator for the fact that, if we as humans take care, maintain social distancing, and avoid crowding, we can be successful at alleviating this contagious disease. </p>
+                <p class="ml-3 grey-color">As technology and business experts, we have the advantage of understanding how technology can help in detecting objects and optimizing processes. This topic is for those of you who would like to come up with solutions for helping with the different safety precautions that have been put up, including but not limited to face mask detection, face mask position correction, recording entry and exit for venues, handwashing posture/time reminders and so on. </p>
+                <p class="ml-3 grey-color">Get creative, and feel free to come up with more safety precautions that technology can help improve! </p>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
         </v-tab-item>
         <v-tab-item>
           <v-card flat>
@@ -151,6 +189,10 @@ export default {
 p, li{
   font-size:1.1rem;
   line-height:1.6rem;
+}
+.grey-color{
+  color: rgba(255, 255, 255, 0.7);
+
 }
   .highlight{
     color: #FF9900; font-weight:bold;
